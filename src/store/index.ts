8d574d711fc00acb711e86b759410ed83fd9f0dc
Natalya Kim <kim87324@yandex.ru>
2025-11-04 22:59:09 +0300

    feat: обновляет конфигурирование хранилища, настраивает middleware thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { appReducer } from './reducer';
+import { createAPI } from '../services/api';
+
+export const api = createAPI();
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      }
+    })
 });
