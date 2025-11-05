cf1c202d71a798989c6e3265ccf97bfae79ad401
Natalya Kim <kim87324@yandex.ru>
2025-11-05 19:04:15 +0300

    feat: добавляет поддержку токенов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
+import { getToken } from './token';
+
 
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -8,6 +10,17 @@ export const createAPI = (): AxiosInstance => {
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['X-Token'] = token;
+    }
+
+    return config;
+  },
+  );
 
   return api;
 };
